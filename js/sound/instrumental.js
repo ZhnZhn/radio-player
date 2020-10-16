563c655ca428dfc3d9adb9af7c46d14d71a6a88b
Yevhenii Molodyi <eugmolody@gmail.com>
2020-10-16 12:57:43 +0300

    Add more radio stations to instrumental topic

--- js/sound/instrumental.js
+++ js/sound/instrumental.js
@@ -2,10 +2,23 @@
 
 exports.__esModule = true;
 exports["default"] = void 0;
-var instrumental = [{
+var instrumental = [
+/*
+{
   title: 'Instrumental Radio',
   src: 'http://178.32.62.163:8599/;stream/1',
   siteUrl: 'http://www.instrumentalradio.eu',
+  br: 128
+},*/
+{
+  title: "EU: Instrumental Radio",
+  src: 'https://str2b.openstream.co/959?aw_0_1st.collectionid=4097&stationId=4097&publisherId=983&k=1602000602',
+  siteUrl: 'https://instrumentalradio.eu',
+  br: 128
+}, {
+  title: 'USA: Beautiful Instrumentals Channel',
+  src: 'https://edge4.peta.live365.net/b37545_128mp3',
+  siteUrl: 'https://www.beautifulinstrumentalschannel.com',
   br: 128
 }, {
   title: 'USA: GotRadio Guitar Genius',
--- src/sound/instrumental.js
+++ src/sound/instrumental.js
@@ -1,10 +1,21 @@
 
 const instrumental = [
+  /*
   {
     title: 'Instrumental Radio',
     src: 'http://178.32.62.163:8599/;stream/1',
     siteUrl: 'http://www.instrumentalradio.eu',
     br: 128
+  },*/{
+    title: "EU: Instrumental Radio",
+    src: 'https://str2b.openstream.co/959?aw_0_1st.collectionid=4097&stationId=4097&publisherId=983&k=1602000602',
+    siteUrl: 'https://instrumentalradio.eu',
+    br: 128
+  },{
+   title: 'USA: Beautiful Instrumentals Channel',
+   src: 'https://edge4.peta.live365.net/b37545_128mp3',
+   siteUrl: 'https://www.beautifulinstrumentalschannel.com',
+   br: 128
   },{
     title: 'USA: GotRadio Guitar Genius',
     src: 'https://pureplay.cdnstream1.com/6018_128.mp3',
