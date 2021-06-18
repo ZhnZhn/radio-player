6cfe5312d30f97118cd4fae365c77f56d86381d5
Yevhenii Molodyi <eugmolody@gmail.com>
2021-06-18 10:52:31 +0300

    Add util crCn

---
+++ src/components/crCn.ts
@@ -0,0 +1,19 @@
+const _isArr = Array.isArray;
+
+type CnType = string | undefined
+type CnTupleType = [boolean, CnType]
+type ItemCnType = CnTupleType | CnType
+
+const _getCn = (arrOrStr: ItemCnType): CnType => _isArr(arrOrStr)
+  ? arrOrStr[0] ? arrOrStr[1] : ''
+  : arrOrStr || '';
+
+const crCn = (...args: Array<ItemCnType>): CnType => {
+  const _cl1 = _getCn(args[0])
+  , _cl2 = _getCn(args[1]);
+  return _cl1
+    ? _cl2 ? `${_cl1} ${_cl2}` : _cl1
+    : _cl2 || void 0 ;
+};
+
+export default crCn
