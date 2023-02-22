1a91374210691130a914d6a38db42b2e4412091e
Yevhenii Molodyi <eugmolody@gmail.com>
2023-02-22 12:10:48 +0200

    Update redux and react-redux dev dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   cache: true,
   entry: {
     lib: [            
-            "preact",             
+            "preact/compat",             
             "preact/hooks",
             "redux", "react-redux",
             "howler"            
