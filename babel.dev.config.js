485111f515f2dae368c5f62a0ad58ebd00d4008c
Yevhenii Molodyi <eugmolody@gmail.com>
2023-07-13 14:57:06 +0300

    Edit babel's configs

--- babel.dev.config.js
+++ babel.dev.config.js
@@ -1,13 +1,28 @@
 module.exports = {
   presets: [
-    "@babel/preset-typescript", 
-    ['@babel/preset-env', { loose: true }],
+    "@babel/preset-typescript",     
+    ['@babel/preset-env', { 
+       loose: true,
+       useBuiltIns: "entry",
+       corejs: "3",
+       //debug: true,      
+       targets: {
+         browsers: [
+            ">0.25%", 
+            /* support arrow-functions */
+            "not ie 11", 
+            "not op_mini all", 
+            "not android 4.4.3-4.4.4", 
+            "not dead"
+         ] 
+       },
+       exclude: []      
+    }],
     ['@babel/preset-react', { "runtime": "automatic" }]
   ],
   plugins: [
     ['react-refresh/babel'],
-    ['@babel/plugin-transform-runtime'],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-runtime'],    
     ['@babel/plugin-transform-react-jsx', {
       runtime: 'automatic',
       importSource: 'preact'      
