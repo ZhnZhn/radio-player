35a2f6c055d74a54ad0ec56b35566770b2deaa23
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-11 10:53:40 +0300

    Edit style of SvgClose function component

--- src/components/zhn/SvgClose.tsx
+++ src/components/zhn/SvgClose.tsx
@@ -1,4 +1,7 @@
-import type { CSSProperties, TabIndexType } from "./types";
+import type { 
+  CSSProperties, 
+  TabIndexType 
+} from "./types";
 
 interface SvgCloseProps {
   className?: string, 
@@ -8,9 +11,9 @@ interface SvgCloseProps {
 }
 
 const CL = "svg-close"
-, STROKE_COLOR = '#f44336'
 , S_SVG: CSSProperties = {
-    padding: 3,
+  padding: 3,
+  stroke: 'inherit'
 };
 
 const SvgClose = ({ 
@@ -25,11 +28,16 @@ const SvgClose = ({
       tabIndex={tabIndex}
       onClick={onClick}
    >
-     <svg viewBox="0 0 12 12" width="100%" height="100%"
-          style={S_SVG} preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
-          strokeWidth="2"
-          stroke={STROKE_COLOR}
-          strokeLinecap="round"
+     <svg 
+        aria-hidden="true"
+        viewBox="0 0 12 12" 
+        width="100%" 
+        height="100%"
+        style={S_SVG} 
+        preserveAspectRatio="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        strokeWidth="2"        
+        strokeLinecap="round"
       >
         <path d="M 0,0 L 12,12" />
         <path d="M 12,0 L 0,12" />
