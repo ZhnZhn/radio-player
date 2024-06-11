58000324c28c3f5f0fbfa5ae7222acaa07192daa
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-11 11:44:34 +0300

    Add Svg function component to CircleButton function component

--- src/components/zhn/CircleButton.tsx
+++ src/components/zhn/CircleButton.tsx
@@ -1,7 +1,9 @@
 import type { WithChildren, CircleButtonProps } from './types';
-import HAS from '../has';
+import type { LegacyRef } from 'react';
+
+import { HAS_TOUCH_EVENT } from '../has';
 import useHotKey from '../hotkeys/useHotKey';
-import { LegacyRef } from 'react';
+import Svg from './Svg';
 
 type CircleButtonType = WithChildren<CircleButtonProps>
 
@@ -17,23 +19,24 @@ const CircleButton = ({
   onClick,
   children
 }: CircleButtonType) => {
-  const _handlers = HAS.TOUCH && onTouchStart
+  const _handlers = HAS_TOUCH_EVENT && onTouchStart
     ? { onTouchStart, onTouchEnd }
     : { onMouseDown, onMouseUp }  
   , _refBt = useHotKey(hotKey, onClick);
   return (
     <button    
+      type="button"
       ref={_refBt as LegacyRef<HTMLButtonElement>}      
       className={CL}      
       aria-label={ariaLabel}
       {..._handlers}
       onClick={onClick}
     >      
-      <svg viewBox="0 0 34 34" width="100%" height="100%">
+      <Svg w="34" width="100%" height="100%">
         <g>
           {children}
         </g>
-      </svg>
+      </Svg>
     </button>
    );
 };
