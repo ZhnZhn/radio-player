faac03e09250036ee5b9de6c1cad09073f4077cd
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-11 11:50:57 +0300

    Add HAS_KEYBOAR_FOCUS named export to componets has

--- src/components/has.ts
+++ src/components/has.ts
@@ -11,3 +11,14 @@ const _isMediaSession = () => navigator
 export const HAS_TRANSITION = _isTransition()
 export const HAS_TOUCH_EVENT = _isTouchEvent()
 export const HAS_MEDIA_SESSION = _isMediaSession()
+
+const getWindowInnerWidth = () => window
+  && window.innerWidth
+const DF_WIDE_WIDTH = 700;
+export const isWideWidth = (
+  wideWidth=DF_WIDE_WIDTH
+) => (getWindowInnerWidth() || wideWidth+1) > wideWidth
+const HAS_WIDE_SCREEN = isWideWidth();
+
+export const HAS_KEYBOARD_FOCUS = !HAS_TOUCH_EVENT
+  || HAS_WIDE_SCREEN
