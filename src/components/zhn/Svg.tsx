f5ef34f456d55429313eace949830bc6c6feb222
Yevhenii Molodyi <eugmolody@gmail.com>
2025-06-11 18:15:19 +0300

    Replace function component Svg JavaScript by TypeScript

--- src/components/zhn/Svg.tsx
+++ src/components/zhn/Svg.tsx
@@ -1,10 +1,20 @@
 
+import type { 
+  ComponentPropsWithoutRef,
+  PropsWithChildren 
+} from "../types";
+
+interface SvgProps extends ComponentPropsWithoutRef<"svg"> {
+    w: number | string;
+    h?: number | string;
+}
+
 const Svg = ({
     w,
     h=w,
     children,
     ...restProps
-}) => (
+}: PropsWithChildren<SvgProps>) => (
     <svg
       aria-hidden="true" 
       viewBox={`0 0 ${w} ${h}`}
@@ -12,7 +22,7 @@ const Svg = ({
       preserveAspectRatio="none" 
       xmlns="http://www.w3.org/2000/svg"      
     >
-        {children}
+      {children}
     </svg>
 );
 
