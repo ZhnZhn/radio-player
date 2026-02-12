ffc57126c7b1495bdd19422b324d83045dc4efd4
Yevhenii Molodyi <eugmolody@gmail.com>
2026-02-12 15:39:54 +0200

    Add function isStr from isTypeFn module to toLink module

--- src/components/zhn/toLink.ts
+++ src/components/zhn/toLink.ts
@@ -1,11 +1,9 @@
+import { isStr } from '../../utils/isTypeFn';
 
 const toLink = (
-    href: string | undefined | null
-): string | void => {
-  const protocol = (href || '').split('://')[0];
-  return protocol === 'https'
-   ? href as string
-   : void 0;
-} 
+  href: unknown
+) => isStr(href) && href.split('://')[0] === 'https'
+  ? href        
+  : void 0;
 
 export default toLink
--- src/utils/isTypeFn.tsx
+++ src/utils/isTypeFn.tsx
@@ -7,5 +7,8 @@ export const isFn = (v: unknown): v is Function =>
 export const isNumber = (n: unknown): n is number => 
   typeof n === 'number' && n-n === 0
 
+export const isStr = (v: unknown): v is string => 
+  typeof v === 'string'
+
 export const isNotEmptyStr = (str?: string) => str
-  && typeof str === 'string'
+  && isStr(str)
