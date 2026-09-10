8caa2e69ea40c83980dd8032530f7a4d94955b33
Yevhenii Molodyi <eugmolody@gmail.com>
2026-09-10 12:25:17 +0300

    Format webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,39 @@
-'use strict'
+"use strict"
 
-const path = require('path')
-    , webpack = require('webpack')
-    , HtmlWebpackPlugin = require('html-webpack-plugin')
-    , babelConfig = require('./babel.config')
-    , TerserPlugin = require('terser-webpack-plugin');
+const path = require("path")
+    , webpack = require("webpack")
+    , HtmlWebpackPlugin = require("html-webpack-plugin")
+    , babelConfig = require("./babel.config")
+    , TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "production",
   cache: true,
   entry: {
     lib: [            
-            "preact/compat",             
-            "preact/hooks",
-            "redux", "react-redux",
-            "howler"            
-         ],
+      "preact/compat",             
+      "preact/hooks",
+      "redux", "react-redux",
+      "howler"            
+    ],
     app: {
-      import: path.resolve('src', 'index.tsx'),
-      dependOn: 'lib'
+      import: path.resolve("src", "index.tsx"),
+      dependOn: "lib"
     }    
   },
   output: {
-      path: path.resolve('app'),
+      path: path.resolve("app"),
       filename: "[name]_[chunkhash].js",
       chunkFilename: "[name]_[chunkhash].js",
-      publicPath: 'app/'
+      publicPath: "app/"
   },
   module: {
     rules: [
-      {
-        //test: /\.jsx?$/,
+      {       
         test: /\.(ts|js)x?$/,  
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
              cacheDirectory: true,
              ...babelConfig  
@@ -49,8 +48,8 @@ module.exports = {
   },
   resolve: {
     symlinks: false,
-    modules: ['local_modules','node_modules'],
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    modules: ["local_modules","node_modules"],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
     "alias": {       
       "react": "preact/compat",      
       "react-dom/test-utils": "preact/test-utils",      
@@ -60,8 +59,8 @@ module.exports = {
   plugins : [        
     new HtmlWebpackPlugin({
         minify: false, 
-        filename: path.resolve('index.html'),
-        template: path.resolve('template', 'index.ejs'),
+        filename: path.resolve("index.html"),
+        template: path.resolve("template", "index.ejs"),
         inject: false        
     })    
   ],  
